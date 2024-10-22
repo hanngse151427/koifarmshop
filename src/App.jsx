f8b45e737e41659f720a28f255ddb259f2d3071f
hanngse151427 <hanngse151427@fpt.edu.vn>
2024-10-22 11:29:41 +0700

    done

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/main/Header";
+import Footer from "./components/main/Footer";
+import HomePage from "./pages/home/HomePage";
+
+
+// import StudentManagement from "./StudentManagement";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <HomePage />
+      <Footer />
+    </>
+
+  );
+}
+
+export default App;
